443a23af548c23a0c922ea1e386644a76948e026
Ushan Dilusha <90477420+ushan10@users.noreply.github.com>
2022-02-13 22:55:15 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,13 @@
 #include <stdio.h>
 
 int main() {
-  
+  int mark1,mark2
+   printf("Enter The first subject marks  : ");
+   scanf("%d", &mark1);
+   printf("Enter The second  subject marks  : ");
+   scanf("%d", &mark2);
+   printf(Total is "%d ",(mark1+mark2));
+    printf(Avarage is "%d ",((mark1+mark2)/2));
   return 0;
 }
 
